ef32a3ce51e91edeebb47fd2eb35652e32a70f15
mich930 <m.kostyk22@gmail.com>
2021-08-26 20:01:49 +0200

    Minor clearups

--- main.py
+++ main.py
@@ -3,18 +3,8 @@ import chess.syzygy
 import random
 import berserk
 import threading
-import time
-from datetime import datetime
 
-#import cProfile
-
-#board = chess.Board('r3k2r/p1ppqpb1/Bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPB1PPP/R3K2R b KQkq - 0 1') #perf test
-#board = chess.Board('1k1r4/ppp1pp2/7p/8/8/8/2qNKPb1/8 w - - 2 36') #checkmate test
-#board = chess.Board('8/1pp2p1k/5n2/1B4p1/4p3/1P2P1P1/3K1P2/8 w - - 1 52') #endgame test
-#board = chess.Board('r1q2k1r/pp3ppp/4bn2/1B6/1P1Bp3/P3P1P1/1Q3P2/3RK1R1 b - - 10 27') #eval test
-#board = chess.Board()
-
-SESSION = berserk.TokenSession('QKIVKEgX1DLKLybP')
+SESSION = berserk.TokenSession('YourTokenGoesHere')
 CLIENT = berserk.Client(session=SESSION)
 CHECKMATE = 20000
 DRAW = 0
@@ -217,51 +207,6 @@ def init_hashing():
         enPassantHash[i] = random.randint(0, 2 ** 64 - 1)
 
 
-def hashing():
-    h = 0
-    for i in range(64):
-        piece = piece_id(i)
-        if not (piece is None):
-            h = h ^ hashTable[i][piece]
-
-    if board.turn == chess.BLACK:
-        h = h ^ nextToMoveHash
-
-    # castling
-    if board.has_kingside_castling_rights(chess.WHITE):
-        h = h ^ castlingHash[0]
-    if board.has_queenside_castling_rights(chess.WHITE):
-        h = h ^ castlingHash[1]
-
-    if board.has_kingside_castling_rights(chess.BLACK):
-        h = h ^ castlingHash[2]
-    if board.has_queenside_castling_rights(chess.BLACK):
-        h = h ^ castlingHash[3]
-
-    #todo - en passant
-
-    print(h)
-    return h
-
-
-def hash_move(move):  # cause it uses xor, hash move is basically the same as hash_unmove, pretty nice trick
-    global hash
-    startingSquare = move.from_square
-    endingSquare = move.to_square
-
-    startingSquarePiece = piece_id(startingSquare)
-    endingSquarePiece = piece_id(endingSquare)
-
-    hash = hash ^ hashTable[startingSquare][startingSquarePiece]
-    hash = hash ^ hashTable[endingSquare][startingSquarePiece]
-    hash = hash ^ nextToMoveHash
-
-    if not (endingSquarePiece is None):
-        hash = hash ^ hashTable[endingSquare][endingSquarePiece]
-
-    #todo - castling
-
-
 def evaluation(board):
     value = 0
     if board.is_checkmate():
@@ -280,7 +225,7 @@ def evaluation(board):
     if castling & chess.BB_A8:
         value -= CASTLE_VALUE
 
-    if len(pieces) <= 5:
+    if len(pieces) <= 5: #endgame table
         with chess.syzygy.open_tablebase("3-4-5piecesSyzygy/3-4-5") as tablebase:
             wdl = tablebase.get_wdl(board)
             dtz = tablebase.get_dtz(board)
@@ -351,17 +296,13 @@ def pre_evaluation(board, move):
         movePreEval = white[fromPiece][toSquare] - white[fromPiece][fromSquare]
     if board.turn == chess.BLACK:
         movePreEval = black[fromPiece][toSquare] - black[fromPiece][fromSquare]
-    #movePreEval = 0
 
     if toPiece is not None:
         toPiece = toPiece-1
-        movePreEval += 10 * pieceScore[toPiece] - pieceScore[fromPiece]  # captures are good (10?)
+        movePreEval += 10 * pieceScore[toPiece] - pieceScore[fromPiece]  # captures are good
 
     if move.promotion is not None:
         movePreEval += 800  # promotions are good
-
-    #if board.is_attacked_by(not board.turn, toSquare):
-        #movePreEval -= pieceScore[fromPiece]  # attacked squares are bad
 
     return movePreEval
 
@@ -403,13 +344,7 @@ def capture_search(board, alpha, beta):
 def seconds(t):
     return t.timestamp()
 
-#init_hashing()
 init_heat_maps()
-
-#hash = hashing()
-#cProfile.run(find_move(MAX_DEPTH, -CHECKMATE, CHECKMATE))
-
-#find_move(MAX_DEPTH, -CHECKMATE, CHECKMATE)
 
 class Game(threading.Thread):
     def __init__(self, client, game_id, **kwargs):
@@ -427,8 +362,6 @@ class Game(threading.Thread):
         self.depth = MAX_DEPTH
 
     def find_move(self, board, depth, alpha, beta):
-        # bestMoveInPosition = {}
-        # fen = board.fen()
 
         if board.is_checkmate():
             return -(CHECKMATE - (MAX_DEPTH - depth))
@@ -464,13 +397,10 @@ class Game(threading.Thread):
 
             if eval > alpha:
                 alpha = eval
-                # bestMoveInPosition = move
                 if depth == self.depth:
                     self.bestMove = move
                     self.bestEval = eval
 
-        # hashedPositions[fen] = (alpha, depth, bestMoveInPosition)
-        #print(self.bestMove)
         return alpha
 
     def run(self):
@@ -500,8 +430,6 @@ class Game(threading.Thread):
         if (moveCount % 2 == 1 and self.ai_turn == chess.BLACK) or (moveCount % 2 == 0 and self.ai_turn == chess.WHITE) or game_state['status'] == 'aborted':
             pass
         else:
-            startTime = time.time()
-            #counter = 0
 
             if self.ai_turn == chess.WHITE:
                 self.ai_time = seconds(game_state['wtime'])
@@ -516,13 +444,7 @@ class Game(threading.Thread):
                 self.depth = 2
 
             self.find_move(self.board, self.depth, -CHECKMATE, CHECKMATE)
-            print(self.bestMove)
             CLIENT.bots.make_move(self.game_id, self.bestMove)
-
-            usedTime = time.time() - startTime
-            self.totalTime += usedTime
-            #print(str(counter) + " nodes evaluated. Time used: " + str(usedTime))
-            #print("Position evaluation: " + str(bestEval / 100))
 
     def handle_chat_line(self, chat_line):
         pass
